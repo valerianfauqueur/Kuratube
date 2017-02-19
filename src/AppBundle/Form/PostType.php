0ba9ea08c99934db32abd07af80d184b5ae52d9e
Valerian Fauqueur <valerian.fauqueur@hetic.net>
2017-02-19 17:09:22 +0100

    [PHP:Post:Form] Remove not user sided input and adapt form input types

--- src/AppBundle/Form/PostType.php
+++ src/AppBundle/Form/PostType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,9 @@ class PostType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')->add('content')->add('author')->add('points')->add('channel')        ;
+        $builder->add('title')
+                ->add('content', TextareaType::class)
+                ->add('channel');
     }
     
     /**
