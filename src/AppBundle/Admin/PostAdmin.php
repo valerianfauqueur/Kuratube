fd535965b52617c187e44b7ce674a2158f503201
Valerian Fauqueur <valerian.fauqueur@hetic.net>
2017-02-23 21:18:25 +0100

    [PHP:Admin:Post] Add video / points and change tags form

--- src/AppBundle/Admin/PostAdmin.php
+++ src/AppBundle/Admin/PostAdmin.php
@@ -7,9 +7,9 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use AppBundle\Form\TagType;
 
 class PostAdmin extends AbstractAdmin
 {
@@ -19,15 +19,21 @@ class PostAdmin extends AbstractAdmin
             ->add('title', 'text')
             ->add('content', TextareaType::class)
             ->add('channel', 'text')
+            ->add('video', 'text')
             ->add('category', EntityType::class, array(
                 'class' => 'AppBundle:Category',
                 'choice_label' => 'name',
             ))
             ->add('tags', CollectionType::class, array(
-                'entry_type' => text,
+                'entry_type' => EntityType::class,
                 'allow_add' => true,
                 'allow_delete' => true,
-            ));
+                'entry_options' => array(
+                    'class' => 'AppBundle:Tag',
+                    'choice_label' => 'name',
+                ),
+            ))
+            ->add('points', NumberType::class);
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
@@ -37,6 +43,7 @@ class PostAdmin extends AbstractAdmin
             ->add('title')
             ->add('content')
             ->add('category.name', null, array( 'label' => 'Category'))
+            ->add('points', 'doctrine_orm_number')
             ->add('created_at', 'doctrine_orm_date_range')
             ->add('updated_at', 'doctrine_orm_date_range');
     }
@@ -47,6 +54,7 @@ class PostAdmin extends AbstractAdmin
             ->addIdentifier('author.username')
             ->addIdentifier('title')
             ->addIdentifier('category')
+            ->addIdentifier('points')
             ->addIdentifier('created_at')
             ->addIdentifier('updated_at');
     }
