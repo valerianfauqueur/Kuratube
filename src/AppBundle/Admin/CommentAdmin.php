cbc614f3cd70ec716fb9e6b38d378bb466d6c045
Valerian Fauqueur <valerian.fauqueur@hetic.net>
2017-02-23 02:36:46 +0100

    [PHP:Admin:Entities] Add dashboard for every entity

--- src/AppBundle/Admin/CommentAdmin.php
+++ src/AppBundle/Admin/CommentAdmin.php
@@ -1,7 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Valérian
- * Date: 23/02/2017
- * Time: 01:19
- */
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CommentAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('comment', TextareaType::class);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('author.username')
+            ->add('post.title')
+            ->add('created_at', 'doctrine_orm_date_range')
+            ->add('updated_at', 'doctrine_orm_date_range');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('author.username')
+            ->addIdentifier('post.title')
+            ->addIdentifier('created_at')
+            ->addIdentifier('updated_at');
+    }
+}
--- src/AppBundle/Admin/UserAdmin.php
+++ src/AppBundle/Admin/UserAdmin.php
@@ -6,24 +6,22 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
-class PostAdmin extends AbstractAdmin
+class UserAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('title', 'text')
-            ->add('content', TextareaType::class)
-            ->add('channel', 'text');
+            ->add('email', EmailType::class)
+            ->add('username', 'text');
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
-            ->add('author.username')
-            ->add('title')
-            ->add('content')
+            ->add('email')
+            ->add('username')
             ->add('created_at', 'doctrine_orm_date_range')
             ->add('updated_at', 'doctrine_orm_date_range');
     }
@@ -31,8 +29,8 @@ class PostAdmin extends AbstractAdmin
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->addIdentifier('author.username')
-            ->addIdentifier('title')
+            ->addIdentifier('email')
+            ->addIdentifier('username')
             ->addIdentifier('created_at')
             ->addIdentifier('updated_at');
     }
